7f154b501e5f08a65850fc517cebf7ecd180f160
lpozoyan <lusinepozoyan@gmail.com>
2023-04-22 14:49:18 +0400

    Добавьте файлы проекта.

---
+++ Dialog_Window/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.DirectoryServices;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Dialog_Window.Models
+{
+    public class Product
+        
+    {
+        [Key]
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        
+        public string Description { get; set; }
+
+        [NotMapped]  public BitmapImage QRCode { get; set; }
+
+        
+    }
+
+
+
+    
+}
